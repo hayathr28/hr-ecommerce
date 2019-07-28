c5281dc82c5f7c4c442781cf38103a95127ad385
sajadreshi <sreshi@1800flowers.com>
2019-07-27 23:38:56 -0400

    checking in remaning files

--- hr-ecommerce-domain/src/main/java/com/hr/ecommerce/operations/EcomOperations.java
+++ hr-ecommerce-domain/src/main/java/com/hr/ecommerce/operations/EcomOperations.java
@@ -3,7 +3,7 @@ package com.hr.ecommerce.operations;
 import com.hr.ecommerce.request.CreateProductRequest;
 
 public interface EcomOperations {
-	public String saveProduct(CreateProductRequest productrequest);
+	public String saveProduct(CreateProductRequest productrequest) throws Exception;
 	public String retrieveProduct();
 	
 	
--- hr-ecommerce-persistence/src/main/java/com/hr/ecommerce/product/MongoProductRepository.java
+++ hr-ecommerce-persistence/src/main/java/com/hr/ecommerce/product/MongoProductRepository.java
@@ -1,8 +1,11 @@
 package com.hr.ecommerce.product;
 
+
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.hr.ecommerce.model.Product;
+
 
 public interface MongoProductRepository extends MongoRepository<Product, String> {
 
